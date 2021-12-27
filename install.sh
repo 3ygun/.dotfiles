9d0ec2f67f241d7fe3b74753025a1bc32a44bb94
David R Soller <dsoller@outlook.com>
2021-12-26 22:22:57 -0500

    [Bash] - Fix install.sh, I don't need the relocation that Tarebyte uses as he uses nesting

--- install.sh
+++ install.sh
@@ -4,11 +4,7 @@
 
 set -e
 
-SCRIPT_SETUP="$0"
-SETUP_DIRECTORY=$(dirname $SCRIPT_SETUP)
-
-cd $SETUP_DIRECTORY/..
 DOTFILES_PATH=$(pwd -P)
 
 ln -sf "${DOTFILES_PATH}/bash/.bash_aliases" "${HOME}/.bash_aliases"
-ln -sf "${DOTFILES_PATH}/bash/.commands.sh" "${HOME}/.commands.sh"
+ln -sf "${DOTFILES_PATH}/bash/commands.sh" "${HOME}/.commands.sh"
