5db9e46a39bf365ded71c6970aeb3e7a12c70890
David R Soller <dsoller@outlook.com>
2021-12-26 21:16:06 -0500

    [Bash] - Update install.sh with Try 4

--- install.sh
+++ install.sh
@@ -2,9 +2,6 @@
 
 # This is for GitHub's Codespaces setup
 
-cp .bash_aliases ~/.bash_aliases
-cp commands.sh ~/commands.sh
-
 set -e
 
 SCRIPT_SETUP="$0"
@@ -13,22 +10,5 @@ SETUP_DIRECTORY=$(dirname $SCRIPT_SETUP)
 cd $SETUP_DIRECTORY/..
 DOTFILES_PATH=$(pwd -P)
 
-FILES_TO_SYMLINK=(config gemrc pryrc tmux tmux-macos.conf tmux.conf)
-
-# If the config directory already exists, then just symlink all of
-# the contents instead.
-if [ -d "${HOME}/.config" ]; then
-	for CONFIG_FILE in $DOTFILES_PATH/config/*; do
-		ln -sfv $CONFIG_FILE $HOME/.config/$(basename $CONFIG_FILE)
-	done
-
-	# Remove config from list since we've symlinked the contents.
-	unset FILES_TO_SYMLINK[0]
-fi
-
 ln -sf "${DOTFILES_PATH}/bash/.bash_aliases" "${HOME}/.bash_aliases"
 ln -sf "${DOTFILES_PATH}/bash/.commands.sh" "${HOME}/.commands.sh"
-
-for FILE in "${FILES_TO_SYMLINK[@]}"; do
-	ln -sf "${DOTFILES_PATH}/${FILE}" "${HOME}/.${FILE}"
-done
